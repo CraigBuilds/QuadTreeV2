f4c8abcbe979eb387a4e75a7f507764af3941e89
Craig <craig.hickman@ukaea.uk>
2023-01-10 21:14:55 +0000

    it's now roking, but it needs a clone

--- src/main.rs
+++ src/main.rs
@@ -17,24 +17,36 @@ fn main() {
         Entity{x: 1, y: 1, width: 2, height: 2, collision: false},
     ];
 
-    let mut tree = QuadTree::new(0,0,128,128); //128x128 world, 8x8 grid, so every leaf is 16x16
-
     loop {
 
-        //rebuild the tree (this does not effect underlying vec capacities)
-        tree.clear();
+        //rebuild the tree
+        let mut tree = QuadTree::new(0,0,128,128); //128x128 world, 8x8 grid, so every leaf is 16x16
         for entity in model.iter() {
             //insert a reference to the entity into the tree
             tree.insert(entity.x, entity.y, entity);
         }
 
-        //calculate colisions
-        for entity in model.iter_mut() {
+        //broad phase
+        let mut checks = vec![];
+        for index in 0..model.len() {
+            let entity = &model[index];
             let leaf = tree.get_leaf_around(entity.x, entity.y).unwrap();
-            for (_,_,other_entity) in leaf.vec.iter() {
-                if is_coliding(entity, other_entity) {
-                    entity.collision = true;
-                }
+            for (_, _, other_entity) in leaf.vec.iter() {
+                //get position of other_entity within the model
+                let other_index = model.iter().position(|e| e as *const Entity == *other_entity as *const Entity).unwrap();
+                checks.push((index, other_index));
+            }
+        }
+
+        //narrow phase
+        for (index, other_index) in checks {
+            let entity = &mut model[index].clone();
+            let other_entity = &mut model[other_index];
+            if is_coliding(entity, other_entity) {
+                other_entity.collision = true;
+            }
+            else {
+                other_entity.collision = false;
             }
         }
     }
